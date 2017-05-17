c2e86387f6d28932c08c5fce07d3b384397e4202
Shumon Chattopadhyay <s.chattopadhyay89@gmail.com>
2017-05-17 11:35:38 -0400

    Adds another Turtle object that draws a circle

--- lotus-drawer.py
+++ lotus-drawer.py
@@ -1,22 +1,26 @@
 import turtle
 
 def draw_square():
-    window = turtle.Screen()
-    window.bgcolor("white")
-
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("blue")
     brad.speed(1)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
 
-    window.exitonclick()
+    for i in range(4):
+        brad.forward(100)
+        brad.right(90)
 
+def draw_circle():
+    angie = turtle.Turtle()
+    angie.shape("turtle")
+    angie.color("red")
+    angie.speed(1)
+
+    angie.circle(100)
+
+    
+window = turtle.Screen()
+window.bgcolor("white")
 draw_square()
+draw_circle()
+window.exitonclick()
